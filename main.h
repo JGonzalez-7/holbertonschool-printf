9c405c845b8ede76c509280952477013575e5541
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-11-20 12:07:55 -0400

    Updated main.h with only the necessary prototypes for printf joint project

--- main.h
+++ main.h
@@ -52,69 +52,6 @@ int print_percent(va_list ap);
 int print_integer(va_list ap);
 
 /**
- * print_binary - prints unsigned integers in binary
- * @ap: argument list containing the integer
- * Return: number of characters written
- */
-int print_binary(va_list ap);
-
-/**
- * print_unsigned - prints unsigned decimal integers
- * @ap: argument list containing the integer
- * Return: number of characters written
- */
-int print_unsigned(va_list ap);
-
-/**
- * print_octal - prints numbers in base 8
- * @ap: argument list containing the integer
- * Return: number of characters written
- */
-int print_octal(va_list ap);
-
-/**
- * print_hex_lower - prints numbers in base 16 lowercase
- * @ap: argument list containing the integer
- * Return: number of characters written
- */
-int print_hex_lower(va_list ap);
-
-/**
- * print_hex_upper - prints numbers in base 16 uppercase
- * @ap: argument list containing the integer
- * Return: number of characters written
- */
-int print_hex_upper(va_list ap);
-
-/**
- * print_S - prints strings with non-printable chars as hex
- * @ap: argument list containing the string
- * Return: number of characters written
- */
-int print_S(va_list ap);
-
-/**
- * print_pointer - prints memory addresses
- * @ap: argument list containing the pointer
- * Return: number of characters written
- */
-int print_pointer(va_list ap);
-
-/**
- * print_reverse - prints strings in reverse
- * @ap: argument list containing the string
- * Return: number of characters written
- */
-int print_reverse(va_list ap);
-
-/**
- * print_rot13 - prints strings encoded in rot13
- * @ap: argument list containing the string
- * Return: number of characters written
- */
-int print_rot13(va_list ap);
-
-/**
  * _putchar - writes a character to stdout
  * @c: character to output
  * Return: number of characters written
@@ -127,14 +64,5 @@ int _putchar(char c);
  * Return: number of characters written
  */
 int print_number(int n);
-
-/**
- * print_base - prints unsigned numbers in any base
- * @num: number to convert
- * @base: target base
- * @uppercase: flag for uppercase letters
- * Return: number of characters written
- */
-int print_base(unsigned long num, int base, int uppercase);
 
 #endif /* MAIN_H */
